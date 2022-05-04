0fabac98c77b22c8143c27e30b1dfce1294dd24c
Deepesh saini <im.deepeshsaini@gmail.com>
2022-05-04 19:25:26 +0530

    second comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,22 @@
 
 class Post < ApplicationRecord
+	belongs_to :user 
+		has_many :comments, as: :commentable, dependent: :destroy
+
+	has_many :likes, as: :post,  dependent: :destroy
+	has_one_attached :avatar
+
+	VALID_STATUSES = ['Public', 'Private']
+	validates :status, presence: true, inclusion: { in: VALID_STATUSES }
+	validates :body, presence: true
 	
+
+	def Private?
+    status == 'Private'
+  end
+
+  def liked?(user)
+  	!!self.likes.find{|like| like.user_id == user.id}
+  end
 
 end
